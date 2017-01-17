ffc89b01177306a984084ed4b36f64c7144634f5
Andy Corrigan <Andy Corrigan>
2017-01-17 15:55:53 -0600

    All ready on my laptop!

--- src/org/usfirst/frc/team6637/robot/HelloWorld.java
+++ src/org/usfirst/frc/team6637/robot/HelloWorld.java
@@ -14,5 +14,7 @@ public class HelloWorld {
 		System.out.println("Are ostriches real?");
 		// Benny's line of code!!!!!1111
 		System.out.println("I am a computer that will have code on it!");
+		System.out.println("Whats Up?");
+		// Andy's second line of code.
 	}
 }
