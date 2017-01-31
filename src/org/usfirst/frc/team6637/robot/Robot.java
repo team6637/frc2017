e3e7f6120cd1b88294870b244a23febc516df2fa
Bradley Jackson <jacksonbrad97@gmail.com>
2017-01-31 18:23:41 -0500

    Compressor Working

--- src/org/usfirst/frc/team6637/robot/Robot.java
+++ src/org/usfirst/frc/team6637/robot/Robot.java
@@ -2,11 +2,15 @@ package org.usfirst.frc.team6637.robot;
 
 // Robot Requirements
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
 
 
 
@@ -27,7 +31,13 @@ public class Robot extends IterativeRobot {
 	Joystick leftStick;
 	int autoLoopCounter;
 	Victor VLeft, VRight;
-	
+	Victor hopper = new Victor(5);
+    Jaguar hopper2 = new Jaguar(6);
+	Joystick gearStick = new Joystick(1);
+	Compressor gearCompressor = new Compressor();
+	Solenoid s1;
+	Solenoid s2;
+	float hopperSpeed = 0.5f;
 	
     /**
      * This function is run when the robot is first started up and should be
@@ -36,27 +46,52 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
     	VLeft = new Victor(2);
     	VRight = new Victor(1);
+    	
+    	gearStick = new Joystick(1);             // USB port
+    	gearCompressor = new Compressor(1);  //Digtial I/O,Relay
+    	gearCompressor.start();                        // Start the air compressor
+
+        s1 = new Solenoid(0);                        // Solenoid port
+        s2 = new Solenoid(1);
+    	
+    	gearCompressor.start();
+    
     	CameraServer.getInstance().startAutomaticCapture();
+    	
     	
     	chasisDrive = new RobotDrive(VLeft,VRight);
     	leftStick = new Joystick(0);	
     }
+    
+    
  
 
     /**
      * Runs the motor from a joystick.
      */
-//    public void operatorControl() {
-//        while (isOperatorControl() && isEnabled()) {
-//        	// Set the motor's output.
-//        	// This takes a number from -1 (100% speed in reverse) to +1 (100% speed going forward)
-//        	motor.set(driveStick.getY());
-//        	
-//            Timer.delay(k_updatePeriod);	// wait 5ms to the next update
-//        }
-//        
-//    }
-//    
+    public void operatorControl() {
+        while (isOperatorControl() && isEnabled()) {
+        	gearCompressor.setClosedLoopControl(true);
+        	gearCompressor.setClosedLoopControl(false);
+        	
+        	// Set the motor's output.
+        	// This takes a number from -1 (100% speed in reverse) to +1 (100% speed going forward)
+        	//chasisDrive.set(leftStick.getY());
+        	 if(gearStick.getRawButton(1) == true)
+             {
+                   s1.set(true);
+                   s2.set(false);
+              }
+              if(gearStick.getRawButton(2) == true)
+              {
+                   s1.set(false);
+                   s2.set(true);
+               }
+            //Timer.delay(1);	// wait 5ms to the next update
+        }
+        
+    }
+    
     
     /**
      * Autonomous Functionality
@@ -91,7 +126,28 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
     	chasisDrive.arcadeDrive(leftStick);
+    	
+//    	if(gearStick.getRawButton(1) == true)
+//        {
+//              s1.set(true);
+//              s2.set(false);
+//         }
+//         if(gearStick.getRawButton(2) == true)
+//         {
+//              s1.set(false);
+//              s2.set(true);
+//          }
+    	
+    	
         //myRobot.mecanumDrive_Cartesian(left., right, null, null);
+    	
+    	if (gearStick.getRawButton(1)) {
+            hopper.set(hopperSpeed);
+        } else if (gearStick.getRawButton(2)) {
+            hopper.set(-hopperSpeed);
+        } else {
+            hopper.set(0.0);
+        }
     }
     
     /**
---
+++ src/org/usfirst/frc/team667/subsystems/Pneumatics.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team667.subsystems;
+
+public class Pneumatics {
+	
+	
+
+}
